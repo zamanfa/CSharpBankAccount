489b37f82293288675b7cad9fa809b8f1a03056f
zamanfa <zamanf123@hotmail.co.uk>
2025-10-23 11:59:15 +0100

    Add project files.

---
+++ BankAccountLibrary/Account.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel.Design;
+
+namespace BankAccountLibrary
+{
+    public class Account
+    {
+        private string Name = "Default Account";
+        private decimal Balance = 0.0m;
+        private string accountNumber = "000000";
+
+        public Account(string name = "Default Account", decimal Balance = 0.0m, string accountNumber = "000000")
+        {
+            this.Name = name;
+            this.Balance = Balance;
+            this.accountNumber = accountNumber;
+        }
+
+        public string AccountName
+        {
+            set
+            {
+                while (value.Length < 10)
+                { 
+                    value = value + "*";
+                }
+                this.Name = value;
+            }
+        
+        }
+
+        public decimal AccountBalance
+        {
+            get { return this.Balance; }
+            set { this.Balance = value; }
+        }
+
+        public string AccountNumber
+        {
+            set
+            {
+                while (value.Length < 10)
+                { 
+                    value = "0" + value;
+                }
+
+                if (value.Length > 10)
+                {
+                    value = value.Substring(value.Length - 10);
+                }
+
+                this.accountNumber = value;
+            }
+        }
+
+        public void Credit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                return;
+            }
+            this.Balance += amount;
+        }
+
+        public void Debit(decimal amount)
+        {
+            if (amount < 0 || amount > this.Balance)
+            {
+                return;
+            }
+            this.Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Account Name: {this.Name}, Account Number: {this.accountNumber}, Balance: {this.Balance:C}";
+        }
+    }
+}
---
+++ BankAccountUsages/Program.cs
@@ -0,0 +1,15 @@
+﻿using BankAccountLibrary;
+
+Account account = new Account("Savings", 10000, "12345678765432");
+
+Console.WriteLine("Account view");
+Console.WriteLine(account.ToString());
+
+account.Debit(3000);
+Console.WriteLine($"\nAfter debiting 3000: {account.AccountBalance}");
+
+account.Debit(-1000);
+Console.WriteLine($"\nAfter debiting -1000: {account.AccountBalance}");
+
+account.Credit(5000);
+Console.WriteLine($"\nAfter crediting 5000: {account.AccountBalance}");
