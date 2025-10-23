8d1cfc669df3d51664d3c30bafa9a4041ee66696
zamanfa <zamanf123@hotmail.co.uk>
2025-10-23 12:52:05 +0100

    adeed tests

---
+++ TestAccount/Test1.cs
@@ -0,0 +1,34 @@
+﻿using BankAccountLibrary;
+using System.Security.Principal;
+
+namespace TestAccount
+{
+    [TestClass]
+    public sealed class TestAccount
+    {
+        [TestMethod]
+        public void TestCredit()
+        {
+            Account account = new Account("Saving", 1000, "10101011");
+            account.Credit(500);
+            Assert.AreEqual(1500, account.AccountBalance);
+        }
+
+        [TestMethod]
+        public void TestDebit()
+        {
+            Account account = new Account("Saving", 1000, "10101011");
+            account.Debit(300);
+            Assert.AreEqual(700, account.AccountBalance);
+        }
+
+        [TestMethod]
+        public void TestDebit_NegativeAmount()
+        {
+            Account account = new Account("Saving", 1000, "10101011");
+            account.Debit(-200);
+            Assert.AreEqual(1000, account.AccountBalance);
+
+        }
+    }
+}
